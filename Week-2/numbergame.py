b052fca0c8aaef78e7eb44830976fc2cb3a17b6e
jlf119 <jlf119@ic.ac.uk>
2019-10-17 14:49:24 +0100

    Numbergame

--- Week-2/numbergame.py
+++ Week-2/numbergame.py
@@ -1,12 +1,25 @@
 
 import random
 # gets a number from the user between 1 and 10
-print('lets play a game - give me a random number from 1 to 10')
-userrand = input()
+print('lets play a game - give me 3 random numbers from 1 to 10')
+userrand1 = input()
+userrand2 = input()
+userrand3 = input()
 # changes the user input from a string to an integer
-userrandfloat = float(userrand)
-userrandint = int(userrandfloat)
+userrandfloat1 = float(userrand1)
+userrandint1 = int(userrandfloat1)
+userrandfloat2 = float(userrand2)
+userrandint2 = int(userrandfloat2)
+userrandfloat3 = float(userrand3)
+userrandint3 = int(userrandfloat3)
 # creates a random number
 comrand = random.randint(1,10)
 # prints if the user input matches the random number with true or false
-print(userrandint == comrand)
+if userrandint1 == comrand:
+    print('true')
+elif userrandint2 == comrand:
+    print('true')
+elif userrandint3 == comrand:
+    print('true')
+else:
+    print('false')
