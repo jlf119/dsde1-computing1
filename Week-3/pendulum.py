6d0eb96b07f2996a39ed80696a0623199f48294c
jlf119 <jlf119@ic.ac.uk>
2019-10-21 18:23:49 +0100

    finalise week 3

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -1,16 +1,13 @@
 import math
 def period(L,g):
-    try:
-        float(L)
-    except:
-        print('new value of L')
-    try:
-        float(g)
-    except:
-        print('new value of g')
-    try:
-        float(1/g)
-    except:
-        print('g should not be 0')
-    T = 2*math.pi * math.sqrt(L/g)
-    return(T)
+    x = isinstance(L,(float,int))
+    y = isinstance(g,(float,int))
+    if x == False:
+       raise(TypeError)
+    elif y == False:
+        raise(TypeError)
+    elif g == 0:
+        raise(ValueError)
+    else: 
+        T = 2*math.pi * math.sqrt(L/g)
+        return(T)
